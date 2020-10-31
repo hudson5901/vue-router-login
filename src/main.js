f7110c01c4cad452a0ded74a0e1684b730a9d971
hudson5901 <rinng.nakamura@gmail.com>
2020-10-31 13:47:24 -0400

    b

--- src/main.js
+++ src/main.js
@@ -6,14 +6,7 @@ import router from './router'
 import firebase from 'firebase'
  
 const config = {
-  apiKey: "AIzaSyA9k4CYioUkv-21Vfy-l-wNCNM_5RhhzQE",
-  authDomain: "mail-test-25981.firebaseapp.com",
-  databaseURL: "https://mail-test-25981.firebaseio.com",
-  projectId: "mail-test-25981",
-  storageBucket: "mail-test-25981.appspot.com",
-  messagingSenderId: "863377271988",
-  appId: "1:863377271988:web:56c19e37fc83399af7fbe1",
-  measurementId: "G-8X17ZQELC3"
+
 };
  
 firebase.initializeApp(config);
